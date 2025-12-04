783101b595cc943b3d90ac1e343711ac0ee20c5a
Javier Asencio <asenciojav42@gmail.com>
2025-12-04 03:33:10 -0500

    fix redis connection and retry

--- src/main/java/ec/tourismvisitplanner/core/CoreApplication.java
+++ src/main/java/ec/tourismvisitplanner/core/CoreApplication.java
@@ -2,8 +2,10 @@ package ec.tourismvisitplanner.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class CoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ec/tourismvisitplanner/core/services/BlacklistTokenService.java
+++ src/main/java/ec/tourismvisitplanner/core/services/BlacklistTokenService.java
@@ -2,9 +2,14 @@ package ec.tourismvisitplanner.core.services;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 import java.time.Duration;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 
 @Service
+@Slf4j
 public class BlacklistTokenService {
     private final StringRedisTemplate redisTemplate;
 
@@ -12,11 +17,24 @@ public class BlacklistTokenService {
         this.redisTemplate = redisTemplate;
     }
 
+    @Retryable(value = { Exception.class }, maxAttempts = 5, backoff = @Backoff(delay = 1000))
     public void blacklistToken(String token, long expirationSeconds) {
         redisTemplate.opsForValue().set(token, "blacklisted", Duration.ofSeconds(expirationSeconds));
     }
 
+    @Recover
+    public void recoverBlacklistToken(Exception e, String token, long expirationSeconds) {
+        log.error("Failed to blacklist token after retries due to Redis error: {}", e.getMessage());
+    }
+
+    @Retryable(value = { Exception.class }, maxAttempts = 5, backoff = @Backoff(delay = 1000))
     public boolean isTokenBlacklisted(String token) {
-        return redisTemplate.hasKey(token);
+        return Boolean.TRUE.equals(redisTemplate.hasKey(token));
+    }
+
+    @Recover
+    public boolean recoverIsTokenBlacklisted(Exception e, String token) {
+        log.error("Failed to check if token is blacklisted after retries due to Redis error: {}", e.getMessage());
+        return false;
     }
 }
